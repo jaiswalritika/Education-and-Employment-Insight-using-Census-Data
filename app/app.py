14d23628d5764dd397f071273ab0bf13bbb0ccbb
Ritika Jaiswal <jaiswalritika047@gmail.com>
2024-12-01 10:32:58 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -197,4 +197,5 @@ def visualize_dist():
     return render_template('visualization_dist.html', data=data_dict)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
